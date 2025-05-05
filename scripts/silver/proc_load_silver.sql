8da8a8b7a268e34138917b1c42a6cc29d7d430a1
harshgoyal961 <164349885+harshgoyal961@users.noreply.github.com>
2025-05-05 13:29:40 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,1 +1,41 @@
+/*
+===============================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. This stored procedure does not accept any parameters or return any values.
 
+Usage Example:
+    CALL silver.load_silver;
+===============================================================================
+*/
+
+INSERT INTO silver.crm_cust_info (cst_id, cst_key, cst_firstname, cst_lastname, cst_marital_status, cst_gndr, cst_create_date)
+SELECT 
+cst_id, 
+cst_key, 
+TRIM(cst_firstname) AS cst_firstname, 
+TRIM(cst_lastname) AS cst_lastname, 
+CASE 
+	WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+	WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	ELSE 'n/a'
+END cst_marital_status,
+CASE 
+	WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	ELSE 'n/a'
+END cst_gndr,
+cst_create_date
+FROM 
+(
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) as Flag_Last
+FROM bronze.crm_cust_info)
+WHERE Flag_Last = 1;
