89bd9fab2c35e11e3959bc6eb4af8932aa6f8492
harshgoyal961 <164349885+harshgoyal961@users.noreply.github.com>
2025-05-05 13:36:54 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -39,3 +39,21 @@ SELECT *,
 ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) as Flag_Last
 FROM bronze.crm_cust_info)
 WHERE Flag_Last = 1;
+
+INSERT INTO silver.crm_prd_info (prd_id, cat_id, prd_key, prd_nm, prd_cost, prd_line, prd_start_dt, prd_end_dt)
+SELECT
+prd_id,
+REPLACE(SUBSTRING(prd_key FROM 1 FOR 5), '-', '_') AS cat_id,
+SUBSTRING(prd_key FROM 7 FOR LENGTH(prd_key)) AS prd_key,
+prd_nm,
+COALESCE(prd_cost, 0) AS prd_cost,
+  CASE UPPER(TRIM(prd_line))
+      WHEN 'M' THEN 'Mountain'
+      WHEN 'R' THEN 'Road'
+      WHEN 'S' THEN 'Other Sales'
+      WHEN 'T' THEN 'Touring'
+      ELSE 'n/a'
+  END AS prd_line,
+CAST(prd_start_dt AS DATE) AS prd_start_dt,
+CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 day' AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info
